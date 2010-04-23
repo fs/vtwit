13564058a6b8ca2ebfe5f2520e07ad4585e7ed79
Timur Vafin <timur.vafin@flatsoft.com>
2010-04-23 13:27:56 +0400

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_CHANGEME_session",
-  :secret => "CHANGESESSION"
+  :key    => "_vtwit_session",
+  :secret => "b7776c13f4f0060236608ee02eb0cdc7"
 }
 
 # Use the database for sessions instead of the cookie-based default,

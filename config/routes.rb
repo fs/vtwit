792082c451e48292b30f7de93ce5bde33b0923ff
Timur Vafin <timur.vafin@flatsoft.com>
2010-04-30 18:30:40 +0400

    Made vk resource root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :vkontakte, :only => [:show, :create, :destroy]
   map.resource :twitter, :only => [:show, :new, :create, :destroy], :collection => {:create => :get }
   map.resource :sync, :only => [:show, :create, :destroy]
+  map.root :vkontakte
 end

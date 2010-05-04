9abf24fc5ba23406760690ebcf9419525db60928
Timur Vafin <timur.vafin@flatsoft.com>
2010-05-04 18:38:41 +0400

    Save vk auth to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ActiveRecord::Base
   class << self
     def register_sync(vk, twitter)
       create(
+        :vk_sid => vk.sid,
+        :vk_mid => vk.mid,
+        :vk_secret => vk.secret,
+        :vk_expire => Time.at(vk.expire.to_i),
+        :vk_username => vk.mid,
         :twitter_handle => twitter.handle,
         :twitter_username => twitter.name,
         :twitter_token => twitter.token,
@@ -15,11 +20,11 @@ class User < ActiveRecord::Base
     end
 
     def sync_exists?(vk, twitter)
-      exists?(:twitter_handle => twitter.handle)
+      exists?(:twitter_handle => twitter.handle, :vk_mid => vk.mid)
     end
 
     def delete_sync(vk, twitter)
-      find(:first, :conditions => {:twitter_handle => twitter.handle}).destroy
+      find(:first, :conditions => {:twitter_handle => twitter.handle, :vk_mid => vk.mid}).destroy
     end
   end
 end
--- db/migrate/20100427145625_create_users.rb
+++ db/migrate/20100427145625_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
+      t.string :vk_sid
+      t.integer :vk_mid
+      t.string :vk_secret
+      t.datetime :vk_expire
+      t.string :vk_username
       t.string :twitter_handle
       t.string :twitter_username
       t.string :twitter_token
